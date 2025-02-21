76dcbfd4c0124e916beab69745856d540efbfd17
olioniT <olioni.tau@gmail.com>
2025-02-22 12:27:45 +1300

    setting up final stuff for starting dev

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,10 @@
+import "./HomePage.scss"
+
+export default function HomePage() {
+    return (
+        <div className="home-page">
+            <h1>Var Devs</h1>
+            <p>Coming soon...</p>
+        </div>
+    )      
+}
