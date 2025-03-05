31d309bcfed05913dbdd669137ebbee63c6b284d
Shubham Singh Rangra <shubhamrangra80@gmail.com>
2025-03-05 22:58:30 +1300

    sorry forgot to make branch, pricing page

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,19 @@ import ServicePage from "./pages/ServicePage";
 import ContactUs from "./pages/ContactUs";
 import NavigationBar from "./components/NavigationBar";
 import React from "react";
-import "./index.scss"
+import "./index.scss";
 import Footer from "./components/Footer";
+import PricingPage from "./pages/PricingPage";
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <NavigationBar/>
+      <NavigationBar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/services" element={<ServicePage />} />
         <Route path="/contact-us" element={<ContactUs />} />
+        <Route path="/price" element={<PricingPage />} />
       </Routes>
       <Footer />
     </div>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -10,26 +10,38 @@ import figmaIcon from "../assets/icons/figma.png";
 import safariIcon from "../assets/icons/safari.png";
 
 import { pageTitle } from "../hooks/pageTitle";
-import { getScreenWidth } from "../hooks/screenWidth"
+import { getScreenWidth } from "../hooks/screenWidth";
 
 export default function HomePage() {
-  const navigate = useNavigate()
-  const screenWidth = getScreenWidth()
+  const navigate = useNavigate();
+  const screenWidth = getScreenWidth();
 
-  const [templates, setTemplates] = useState<{top: string[]; bottom: string[]}>({top: [], bottom: []})
+  const [templates, setTemplates] = useState<{
+    top: string[];
+    bottom: string[];
+  }>({ top: [], bottom: [] });
 
   useEffect(() => {
-    const templates = Object.keys(import.meta.glob("/src/assets/templates/*.{png,jpg,jpeg,svg}"))
-    const topShuffleOrder = [...Array(8).keys()].sort(() => Math.random() - 0.5)
-    const bottomShuffleOrder = [...Array(8).keys()].sort(() => Math.random() - 0.5)
+    const templates = Object.keys(
+      import.meta.glob("/src/assets/templates/*.{png,jpg,jpeg,svg}")
+    );
+    const topShuffleOrder = [...Array(8).keys()].sort(
+      () => Math.random() - 0.5
+    );
+    const bottomShuffleOrder = [...Array(8).keys()].sort(
+      () => Math.random() - 0.5
+    );
 
-    const topTemplates = topShuffleOrder.map(i => templates[i]).concat(topShuffleOrder.map(i => templates[i]))
-    const bottomTemplates = bottomShuffleOrder.map(i => templates[i]).concat(bottomShuffleOrder.map(i => templates[i]))
+    const topTemplates = topShuffleOrder
+      .map((i) => templates[i])
+      .concat(topShuffleOrder.map((i) => templates[i]));
+    const bottomTemplates = bottomShuffleOrder
+      .map((i) => templates[i])
+      .concat(bottomShuffleOrder.map((i) => templates[i]));
     setTemplates({
-      "top": topTemplates,
-      "bottom": bottomTemplates
-    })
-
+      top: topTemplates,
+      bottom: bottomTemplates
+    });
   }, []);
 
   pageTitle("Welcome!");
@@ -39,120 +51,159 @@ export default function HomePage() {
       <div className="templates-container">
         <div className="template-row top-row">
           <span>
-            { templates["top"].map((template, index) => (
-              <img key={`template-${index}`} src={template} className="template" />
+            {templates["top"].map((template, index) => (
+              <img
+                key={`template-${index}`}
+                src={template}
+                className="template"
+              />
             ))}
           </span>
         </div>
         <div className="template-row bottom-row">
           <span>
-            { templates["bottom"].map((template, index) => (
-              <img key={`template-${index}`} src={template} className="template" />
+            {templates["bottom"].map((template, index) => (
+              <img
+                key={`template-${index}`}
+                src={template}
+                className="template"
+              />
             ))}
           </span>
         </div>
       </div>
-      { screenWidth > 1200 ? (
-          <div className="structure-container">
-            <div className="subheader-box">
-              <h2 id="your-idea">Your IDEA 💡</h2>
+      {screenWidth > 1200 ? (
+        <div className="structure-container">
+          <div className="subheader-box">
+            <h2 id="your-idea">Your IDEA 💡</h2>
+          </div>
+          <div className="structure-subbox">
+            <div className="column-left">
+              <h1 id="work-structure">Work Structure</h1>
             </div>
-            <div className="structure-subbox">
-              <div className="column-left">
-                <h1 id="work-structure">Work Structure</h1>
+            <div className="column-right column">
+              <div className="header-box">
+                <h1 id="is-our-goal">Is our GOAL 🎯</h1>
               </div>
-              <div className="column-right column">
-                <div className="header-box">
-                  <h1 id="is-our-goal">Is our GOAL 🎯</h1>
-                </div>
-                <div className="structure-items">
-                  <div className="row top-row">
-                    <div className="item box-1">
-                      <span>
-                        <h2>Talk</h2>
-                        <img src={chatIcon}/>
-                      </span>
-                      <p>
-                      Every great website starts with a conversation. We begin by understanding your vision, goals, and what makes your brand unique. Whether you're starting from scratch or looking to revamp an existing site, we'll guide you through the process, ensuring your ideas take shape in a way that's both visually stunning and strategically effective.
-                      </p>
-                    </div>
-                    <div className="item box-2">
+              <div className="structure-items">
+                <div className="row top-row">
+                  <div className="item box-1">
                     <span>
-                        <h2>Design</h2>
-                        <img src={figmaIcon}/>
-                      </span>
-                      <p>
-                        Once we understand your needs, we move to the design phase. This is where your vision takes shape. We create a clean, modern, and user-friendly design tailored to your business. Every element, from colors to layout, is chosen to enhance the user experience and match your brand identity.
-                      </p>
-                    </div>
+                      <h2>Talk</h2>
+                      <img src={chatIcon} />
+                    </span>
+                    <p>
+                      Every great website starts with a conversation. We begin
+                      by understanding your vision, goals, and what makes your
+                      brand unique. Whether you're starting from scratch or
+                      looking to revamp an existing site, we'll guide you
+                      through the process, ensuring your ideas take shape in a
+                      way that's both visually stunning and strategically
+                      effective.
+                    </p>
                   </div>
-                  <div className="row bottom-row">
-                    <div className="item box-3">
+                  <div className="item box-2">
                     <span>
-                        <h2>Create</h2>
-                        <img src={safariIcon}/>
-                      </span>
-                      <p>
-                      With the design approved, we start building your website. We use the latest technologies to ensure fast loading times, smooth navigation, and mobile responsiveness. Whether it's a static website or a dynamic platform, we develop it with efficiency and security in mind. Once everything is ready, we launch your website and make sure it works flawlessly.
-                      </p>
-                    </div>
+                      <h2>Design</h2>
+                      <img src={figmaIcon} />
+                    </span>
+                    <p>
+                      Once we understand your needs, we move to the design
+                      phase. This is where your vision takes shape. We create a
+                      clean, modern, and user-friendly design tailored to your
+                      business. Every element, from colors to layout, is chosen
+                      to enhance the user experience and match your brand
+                      identity.
+                    </p>
+                  </div>
+                </div>
+                <div className="row bottom-row">
+                  <div className="item box-3">
+                    <span>
+                      <h2>Create</h2>
+                      <img src={safariIcon} />
+                    </span>
+                    <p>
+                      With the design approved, we start building your website.
+                      We use the latest technologies to ensure fast loading
+                      times, smooth navigation, and mobile responsiveness.
+                      Whether it's a static website or a dynamic platform, we
+                      develop it with efficiency and security in mind. Once
+                      everything is ready, we launch your website and make sure
+                      it works flawlessly.
+                    </p>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        ) : (
-          <div className="structure-container">
-            <div className="subheader-box">
-              <h1 id="work-structure">Our Work Structure</h1>
-            </div>
-            <div className="structure-subbox">
-              <div className="column">
-                <div className="structure-items">
-                  <div className="row">
-                    <div className="item box-1">
-                      <span>
-                        <h2>Talk</h2>
-                        <img src={chatIcon}/>
-                      </span>
-                      <p>
-                      Every great website starts with a conversation. We begin by understanding your vision, goals, and what makes your brand unique. Whether you're starting from scratch or looking to revamp an existing site, we'll guide you through the process, ensuring your ideas take shape in a way that's both visually stunning and strategically effective.
-                      </p>
-                    </div>
-                    <div className="item box-2">
+        </div>
+      ) : (
+        <div className="structure-container">
+          <div className="subheader-box">
+            <h1 id="work-structure">Our Work Structure</h1>
+          </div>
+          <div className="structure-subbox">
+            <div className="column">
+              <div className="structure-items">
+                <div className="row">
+                  <div className="item box-1">
                     <span>
-                        <h2>Design</h2>
-                        <img src={figmaIcon}/>
-                      </span>
-                      <p>
-                        Once we understand your needs, we move to the design phase. This is where your vision takes shape. We create a clean, modern, and user-friendly design tailored to your business. Every element, from colors to layout, is chosen to enhance the user experience and match your brand identity.
-                      </p>
-                    </div>
-                    <div className="item box-3">
+                      <h2>Talk</h2>
+                      <img src={chatIcon} />
+                    </span>
+                    <p>
+                      Every great website starts with a conversation. We begin
+                      by understanding your vision, goals, and what makes your
+                      brand unique. Whether you're starting from scratch or
+                      looking to revamp an existing site, we'll guide you
+                      through the process, ensuring your ideas take shape in a
+                      way that's both visually stunning and strategically
+                      effective.
+                    </p>
+                  </div>
+                  <div className="item box-2">
                     <span>
-                        <h2>Create</h2>
-                        <img src={safariIcon}/>
-                      </span>
-                      <p>
-                      With the design approved, we start building your website. We use the latest technologies to ensure fast loading times, smooth navigation, and mobile responsiveness. Whether it's a static website or a dynamic platform, we develop it with efficiency and security in mind. Once everything is ready, we launch your website and make sure it works flawlessly.
-                      </p>
-                    </div>
+                      <h2>Design</h2>
+                      <img src={figmaIcon} />
+                    </span>
+                    <p>
+                      Once we understand your needs, we move to the design
+                      phase. This is where your vision takes shape. We create a
+                      clean, modern, and user-friendly design tailored to your
+                      business. Every element, from colors to layout, is chosen
+                      to enhance the user experience and match your brand
+                      identity.
+                    </p>
+                  </div>
+                  <div className="item box-3">
+                    <span>
+                      <h2>Create</h2>
+                      <img src={safariIcon} />
+                    </span>
+                    <p>
+                      With the design approved, we start building your website.
+                      We use the latest technologies to ensure fast loading
+                      times, smooth navigation, and mobile responsiveness.
+                      Whether it's a static website or a dynamic platform, we
+                      develop it with efficiency and security in mind. Once
+                      everything is ready, we launch your website and make sure
+                      it works flawlessly.
+                    </p>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        )
-      }
+        </div>
+      )}
       <div className="footer">
-        <p>
-          &copy; VarDevs { Date().split(" ")[3] }
-        </p>
+        <p>&copy; VarDevs {Date().split(" ")[3]}</p>
       </div>
 
       <div className="contact-us">
         <button onClick={() => navigate("/contact-us")}>
-            <ChatBubbleIcon id="chat-bubble"/>
+          <ChatBubbleIcon id="chat-bubble" />
         </button>
       </div>
     </div>
